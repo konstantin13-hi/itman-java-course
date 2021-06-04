faf783a992d91942005a1ed9d355611eb1d1b246
konstantin13-hi <kostya.chizhevskiy@mail.ru>
2021-06-04 18:51:06 +0200

    make a tast

--- src/test/java/tasks/Task406Tests.java
+++ src/test/java/tasks/Task406Tests.java
@@ -4,7 +4,6 @@ package tasks;
 public class Task406Tests {
     @Test
     public void sumPositiveValues() {
-        int[] a = new int[]{5, 3, 1, -1, 0};
         Assertions.assertEquals(3, Task406.findSecondMax(new int[]{3,2,4,32,32,4,5,10,10}) );
     }
 
