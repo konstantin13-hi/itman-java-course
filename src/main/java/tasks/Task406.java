b5b50ba636d37383d9916e35c7ceb5ca83d427ef
konstantin13-hi <kostya.chizhevskiy@mail.ru>
2021-06-04 21:41:22 +0200

    new task12

--- src/main/java/tasks/Task406.java
+++ src/main/java/tasks/Task406.java
@@ -2,19 +2,19 @@ package tasks;
 
 public class Task406 {
     public static int findSecondMax(int[]array){
-        int second=array[array.length-2];
         for(int i = 0; i<array.length;i++){
             int minIndex = i;
-            for (int j=1;j<array.length;j++){
+            for (int j=i+1;j<array.length;j++){
                 if(array[minIndex]>array[j]){
                     minIndex= j;
-
                 }
+            }
                     int t = array[minIndex];
                     array[minIndex] = array[i];
                     array[i]= t;
-                }
+
             }
+        int second=array[array.length-2];
 
 
         return second;
