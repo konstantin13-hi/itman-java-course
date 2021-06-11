9dd432fb850da0af381f64f203452486b6b5c0ed
konstantin13-hi <kostya.chizhevskiy@mail.ru>
2021-06-11 20:08:56 +0200

    new EVENT2

--- src/test/java/tasks/Task408Tests.java
+++ src/test/java/tasks/Task408Tests.java
@@ -9,14 +9,17 @@ public class Task408Tests {
     @Test
     public void sumPositiveValues() {
 
-        Event[] events = new Event[]{
+        Event[] actual = new Event[]{
                 new Event(1994,11,12, "sae1"),
                 new Event(1994,2,4,"sa3"),
                 new Event(31,3,5,"rew")
         };
-        Event expectedLink = events[0];
-        Event actual = Task439.findLast(events);
-        Assertions.assertSame(expectedLink, actual);
+        Event expected =actual[0];
+        Event expected1 =actual[2];
+
+        Task408.sort(actual);
+        Assertions.assertEquals(expected, actual[2]);
+        Assertions.assertEquals(expected1, actual[0]);
 
 
     }
