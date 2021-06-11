9c28bd04a301f11d558f6c46172f69060aba9bbe
konstantin13-hi <kostya.chizhevskiy@mail.ru>
2021-06-11 19:46:32 +0200

    new EVENT1

--- src/test/java/tasks/Task407Tests.java
+++ src/test/java/tasks/Task407Tests.java
@@ -11,10 +11,19 @@ public class Task407Tests {
         int[][] a = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         int[] b =a[2];//
         Task407.sortMatrixRowsDesc(a);
-
-
-
         Assertions.assertSame(b, a[0]);
+
+    }
+
+    @Test
+    public void sortMatrixRowsDesc1() {
+        int[][] a = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] b = new int[][]{{7, 8, 9}, {4, 5, 6},{1, 2, 3}};//
+
+        Task407.sortMatrixRowsDesc(a);
+
+
+        Assertions.assertArrayEquals(b, a);
 
     }
 
