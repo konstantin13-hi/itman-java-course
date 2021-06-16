e729b92ffa67be135272d247f8a35acbf45c1d71
konstantin13-hi <kostya.chizhevskiy@mail.ru>
2021-06-16 19:27:46 +0200

    ewq

--- src/main/java/tasks/Task433.java
+++ src/main/java/tasks/Task433.java
@@ -3,9 +3,10 @@ package tasks;
 public class Task433 {
     public static long gcd(long x,long y){
         long c=0;
-        for (int i= 1;i<x+1;i++){
+        for (long i= x;i!=0;i--){
             if (x%i==0 &&(y%i)==0){
                 c=i;
+                break;
             }
         }
 
