9ee4efcfe09a774e206c9fa3c9aaf6bca5a93370
konstantin13-hi <kostya.chizhevskiy@mail.ru>
2021-06-16 19:21:41 +0200

    f6

--- src/main/java/tasks/Task433.java
+++ src/main/java/tasks/Task433.java
@@ -2,48 +2,13 @@ package tasks;
 
 public class Task433 {
     public static long gcd(long x,long y){
-        long []a=new long[1000000];
-        long []b=new long[1000000];
-        int a1=0;
-        int b1=0;
         long c=0;
-        int h1=0;
-        for(int i=1;i<x+1;i++){
-            if(x%i==0){
-                a[h1]=i;
-                h1++;
-                a1++;
+        for (int i= 1;i<x+1;i++){
+            if (x%i==0 &&(y%i)==0){
+                c=i;
             }
         }
-        int h=0;
-        for(int i=1;i<y+1;i++){
-            if(y%i==0){
-                b[h]=i;
-                h++;
-                b1++;
-            }
-        }
-        long[]a3=new long[a1];
-        long[]b3=new long[b1];
-        for (int i=0;i<a3.length;i++ ){
-            a3[i]=a[i];
-        }
-        for (int i=0;i<b3.length;i++ ){
-            b3[i]=b[i];
-        }
-        int cg=0;
-        for (int i=a3.length-1;i!=-1;i--){
-            if (cg>0){
-                break;
-            }
-            for (int j=b3.length-1;j!=-1;j--){
-                if(a3[i]==b3[j]){
-                    c=a3[i];
-                    cg++;
-                    break;
-                }
 
-        }}
         return c;
     }
 }
