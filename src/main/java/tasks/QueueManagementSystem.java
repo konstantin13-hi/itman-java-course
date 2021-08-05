d02da6ea76c3e809d25b096f5f608c69113f56cd
konstantin13-hi <kostya.chizhevskiy@mail.ru>
2021-08-05 12:35:01 +0200

    8

--- src/main/java/tasks/QueueManagementSystem.java
+++ src/main/java/tasks/QueueManagementSystem.java
@@ -5,16 +5,12 @@ import entities.Ticket;
 public class QueueManagementSystem {
 
     public String place;
-    public int currentNumber=1;
+    public int currentNumber = 1;
+    Ticket myticket;
 
-    Ticket myticket ;
-
-
-    public  QueueManagementSystem(String place) {
-
-        this.place =place;
-        myticket = new Ticket(currentNumber, this.place);
-
+    public QueueManagementSystem(String place) {
+        this.place = place;
+        myticket = new Ticket(currentNumber, place);
 
     }
 
@@ -22,10 +18,8 @@ public class QueueManagementSystem {
         currentNumber++;
         return myticket;
     }
-    public int getTotalTickets(){
+
+    public int getTotalTickets() {
         return getNextTicket().number;
     }
-
-
-
 }
