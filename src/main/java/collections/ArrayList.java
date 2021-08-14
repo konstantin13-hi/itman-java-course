e5725bc7691e8663fa34675617dff7240ec881a2
konstantin13-hi <kostya.chizhevskiy@mail.ru>
2021-08-15 01:33:08 +0200

    242

---
+++ src/main/java/collections/ArrayList.java
@@ -0,0 +1,34 @@
+package collections;
+
+public class ArrayList {
+
+
+    private int capacity;
+
+
+    public ArrayList(int capacity){
+        this.capacity=capacity;
+
+
+    }
+
+    int[]array;
+
+    public int getCapacity(){
+        return capacity;
+    }
+    public void set(int index, int element){
+
+       array= new int[getCapacity()];
+        array[index]=element;
+    }
+
+    public  int get(int index) {
+        return array[index];
+    }
+    public int size(){
+        return capacity;
+    }
+
+
+}
---
+++ src/test/java/collections/ArrayListTest.java
@@ -0,0 +1,32 @@
+package collections;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+class ArrayListTest {
+
+    @Test
+    void size() {
+        ArrayList arrayList = new ArrayList(4);
+        Assertions.assertEquals(4,arrayList.size());
+    }
+
+    @Test
+    void get() {
+        ArrayList arrayList = new ArrayList(4);
+        arrayList.set(0,1);
+        Assertions.assertEquals(1,arrayList.get(0));
+    }
+
+    @Test
+    void get2() {
+        ArrayList arrayList = new ArrayList(100);
+        arrayList.set(20,33);
+        Assertions.assertEquals(33,arrayList.get(20));
+    }
+
+
+
+
+}
