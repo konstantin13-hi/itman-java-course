171d4170d772bd5bd47e51dfd85941a0ec3cd0e5
konstantin13-hi <kostya.chizhevskiy@mail.ru>
2021-08-23 19:36:07 +0200

    132

--- src/main/java/tasks/QueueManagementSystem.java
+++ src/main/java/tasks/QueueManagementSystem.java
@@ -1,16 +1,20 @@
 package tasks;
 
+import collections.ArrayList;
 import entities.Ticket;
 
 public class QueueManagementSystem {
 
-
+    static ArrayList arrayList;
     private int currentNumber ;
+    private int index;
     String place;
+
 
 
     public QueueManagementSystem(String a) {
         this.place=a;
+        arrayList = new ArrayList(1);
     }
 
     public Ticket getNextTicket() {
@@ -22,4 +26,19 @@ public class QueueManagementSystem {
 
         return currentNumber;
     }
+
+    public void toNextWorkDay(){
+        index++;
+        getVisitsByDay();
+
+
+
+    }
+
+   public ArrayList getVisitsByDay(){
+        arrayList.add(currentNumber);
+        currentNumber=0;
+
+        return arrayList;
+   }
 }
--- src/test/java/tasks/QueueManagementSystemTest.java
+++ src/test/java/tasks/QueueManagementSystemTest.java
@@ -17,17 +17,31 @@ class QueueManagementSystemTest {
 
 
     }
+
     @Test
     public void Queue() {
         QueueManagementSystem my = new QueueManagementSystem("Bank");
         Ticket B = my.getNextTicket();
         Ticket B1 = my.getNextTicket();
         Ticket B2 = my.getNextTicket();
-        Assertions.assertEquals(1,B.getNumber());
-        Assertions.assertEquals(2,B1.getNumber());
-        Assertions.assertEquals(3,B2.getNumber());
-        Assertions.assertEquals(3,my.getTotalTickets());
+        Assertions.assertEquals(1, B.getNumber());
+        Assertions.assertEquals(2, B1.getNumber());
+        Assertions.assertEquals(3, B2.getNumber());
+        Assertions.assertEquals(3, my.getTotalTickets());
+    }
+
+    @Test
+    public void getVisitsByDay(){
+        QueueManagementSystem system = new QueueManagementSystem("Bank");
+        system.getNextTicket();
+        system.getNextTicket();
+        system.toNextWorkDay();
+        system.getNextTicket();
+        system.toNextWorkDay();
+        Assertions.assertEquals(2,system.getVisitsByDay().get(0));
+        Assertions.assertEquals(1,system.getVisitsByDay().get(1));
+        Assertions.assertEquals(0,system.getVisitsByDay().get(2));
+    }
 
 
-
-}}
+}
