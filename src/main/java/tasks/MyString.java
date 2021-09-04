476badc4a7ee7d6de9fda65665a07bfa71c3980e
konstantin13-hi <kostya.chizhevskiy@mail.ru>
2021-09-04 19:59:53 +0200

    ew432

--- src/main/java/tasks/MyString.java
+++ src/main/java/tasks/MyString.java
@@ -1,5 +1,7 @@
 package tasks;
 
+import java.util.Arrays;
+
 public class MyString {
     char[] chars;
     public MyString(char[]a){
@@ -18,5 +20,26 @@ public class MyString {
     public char charAt(int index){
         return chars[index];
     }
+
+    public int compareTo(MyString that){
+        int result=0;
+        if (that==null){
+            return 1;
+        }
+       if( that.chars.length>chars.length){
+           result=-1;
+       }
+       if ( that.chars.length<chars.length){
+           result= 1;
+       }
+        return result;
+    }
+
+   public boolean equals(MyString that){
+        if(that==null){
+            return false;
+        }
+        return Arrays.equals(that.chars, chars);
+   }
 
 }
--- src/test/java/tasks/MyStringTest.java
+++ src/test/java/tasks/MyStringTest.java
@@ -18,14 +18,64 @@ class MyStringTest {
 
     @Test
     public void charAt(){
-
         char[]chars=new char[]{'H', 'e', 'l', 'l', 'o'};
         MyString a = new MyString(chars);
-
         Assertions.assertEquals('H',a.charAt(0));
 
+    }
 
+    @Test
+    public void compareTo(){
+        char[]chars=new char[]{'H', 'e', 'l', 'l', 'o'};
+        MyString a = new MyString(chars);
+        char[]ch=new char[]{'H', 'e', 'l', 'l', 'o'};
+        MyString b = new MyString(chars);
+        Assertions.assertEquals(0,a.compareTo(b));
+    }
 
+    @Test
+    public void compareTo12(){
+        char[]chars=new char[]{'H', 'e', 'l', 'l', 'o'};
+        MyString a = new MyString(chars);
+        char[]ch=new char[]{'H', 'e', 'l', 'l'};
+        MyString b = new MyString(ch);
+        Assertions.assertEquals(1,a.compareTo(b));
+    }
+
+    @Test
+    public void compareTo1(){
+        char[]chars=new char[]{'H', 'e'};
+        MyString a = new MyString(chars);
+        char[]ch=new char[]{'H', 'e', 'l', 'l'};
+        MyString b = new MyString(ch);
+        Assertions.assertEquals(-1,a.compareTo(b));
+    }
+
+    @Test
+    public void equals(){
+        char[]chars=new char[]{'H', 'e', 'l', 'l', 'o'};
+        MyString a = new MyString(chars);
+        char[]ch=new char[]{'H', 'e', 'l', 'l'};
+        MyString b = new MyString(ch);
+        Assertions.assertFalse(a.equals(b));
+    }
+
+    @Test
+    public void equals1(){
+        char[]chars=new char[]{'H', 'e', 'l', 'l', 'o'};
+        MyString a = new MyString(chars);
+        char[]ch=new char[]{'H', 'e', 'l', 'l', 'o'};
+        MyString b = new MyString(ch);
+        Assertions.assertTrue(a.equals(b));
+    }
+
+    @Test
+    public void equals132(){
+        char[]chars=new char[]{'H', 'e', 'l', 'l', 'o'};
+        MyString a = new MyString(chars);
+        char[]ch=new char[]{'H', 'e', 'l', 'l', 'D'};
+        MyString b = new MyString(ch);
+        Assertions.assertFalse(a.equals(b));
     }
 
 }
