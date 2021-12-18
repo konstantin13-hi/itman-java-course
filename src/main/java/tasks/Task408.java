997a5a500456c25f84d4b6182312537fe33c3fd6
konstantin13-hi <kostya.chizhevskiy@mail.ru>
2021-12-18 18:02:46 +0100

    34234

--- src/main/java/tasks/Task408.java
+++ src/main/java/tasks/Task408.java
@@ -13,7 +13,7 @@ public class Task408 {
      * @cpu O(n ^ 2)
      * @ram O(1)
      */
-    public static void bubbleSort(Event[] events) {
+    public static void sort(Event[] events) {
         for (int i = 0; i < events.length; i++) {
             for (int j = 1; j < events.length; j++) {
                 if ((events[j - 1].compareTo(events[j]) > 0)) {
