f6080cbec17b1546411175dd27a49dcb352e9607
konstantin13-hi <kostya.chizhevskiy@mail.ru>
2022-01-09 19:31:17 +0100

    42

--- src/main/java/utils/ArrayUtils.java
+++ src/main/java/utils/ArrayUtils.java
@@ -428,50 +428,52 @@ public class ArrayUtils {
     }
   public static   void mergeSort(int[] array, int fromIndex, int toIndex){
           int[] t = new int[array.length];
-          for (int size = 1; size < toIndex-fromIndex; size *= 2) {
+          for (int size = 1; size <= toIndex-fromIndex; size *= 2) {
               System.arraycopy(array, 0, t, 0, array.length);
-              for (int l = fromIndex; l + size < toIndex; l += 2 * size) {
+              for (int l = fromIndex; l + size <= toIndex; l += 2 * size) {
                   int m = l + size;
                   int r = m + size;
-                  if (r > array.length) {
-                      r = array.length;
+                  if (r > toIndex) {
+                      r = toIndex;
                   }
                   int i = l;
                   int j = m;
                   int k = l;
-                  while (i < m && j < r) {
+                  while (i < m && j <= r) {
                       array[k++] = t[i] <= t[j] ? t[i++] : t[j++];
                   }
                   while (i < m) {
                       array[k++] = t[i++];
                   }
-                  while (j < r) {
+                  while (j <= r) {
                       array[k++] = t[j++];
                   }
               }
           }
       }
+      //10 9 7 8 11
+      //
 
     public static   void mergeSort(Event[] array, int fromIndex, int toIndex){
         Event[] t = new Event[array.length];
-        for (int size = 1; size < toIndex-fromIndex; size *= 2) {
+        for (int size = 1; size <= toIndex-fromIndex; size *= 2) {
             System.arraycopy(array, 0, t, 0, array.length);
-            for (int l = fromIndex; l + size < toIndex; l += 2 * size) {
+            for (int l = fromIndex; l + size <= toIndex; l += 2 * size) {
                 int m = l + size;
                 int r = m + size;
-                if (r > array.length) {
-                    r = array.length;
+                if (r > toIndex) {
+                    r = toIndex;
                 }
                 int i = l;
                 int j = m;
                 int k = l;
-                while (i < m && j < r) {
-                    array[k++] = t[i].compareTo(t[j])!=1 ? t[i++] : t[j++];
+                while (i < m && j <= r) {
+                    array[k++] = t[i].compareTo(t[j])<=0 ? t[i++] : t[j++];
                 }
                 while (i < m) {
                     array[k++] = t[i++];
                 }
-                while (j < r) {
+                while (j <= r) {
                     array[k++] = t[j++];
                 }
             }
