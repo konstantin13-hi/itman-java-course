0d9431da3b1fa88e2ede3431b0aa9c97966fc23f
konstantin13-hi <kostya.chizhevskiy@mail.ru>
2022-03-12 13:36:32 +0100

    springWeb

--- src/main/java/services/example/ExampleController.java
+++ src/main/java/services/example/ExampleController.java
@@ -1,16 +1,39 @@
 package services.example;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ExampleController {
     @GetMapping("/")
+    /**
+     * @cpu O(1)
+     * @ram O(1)
+     */
     public String index() {
         return "I'm your first web-service";
     }
     @GetMapping("/second")
+    /**
+     * @cpu O(1)
+     * @ram O(1)
+     */
     public String indexSecond() {
         return "I'm the second binding";
+    }
+    @PostMapping("/second")
+    public String indexPost() {
+        return "I'm post mapping";
+    }
+    @PutMapping("/second")
+    public String indexPut() {
+        return "I'm put mapping";
+    }
+    @PatchMapping("/second")
+    public String indexPatch() {
+        return "I'm patch mapping";
+    }
+    @DeleteMapping("/second")
+    public String indexDelete() {
+        return "I'm patch mapping";
     }
 }
