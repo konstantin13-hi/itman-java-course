b7988c0f17c9dc91047607798733e6871d5e1ee0
konstantin13-hi <kostya.chizhevskiy@mail.ru>
2022-06-30 21:59:43 +0200

    Node

--- src/main/java/collections/LinkedList.java
+++ src/main/java/collections/LinkedList.java
@@ -4,39 +4,75 @@ import entities.Node;
 
 public class LinkedList {
     private Node head;
-  public  LinkedList(){
-      head=null;
-  }
+
+    public LinkedList() {
+        head = null;
+    }
 
 
-
-
-    public void addFirst(int element){
-        head= new Node(element,head);
+    public void addFirst(int element) {
+        head = new Node(element, head);
 
     }
-   public  int getFirst(){
-      return   head.getElement();
+
+    public int getFirst() {
+        return head.getElement();
     }
-   public  int removeFirst(){
+
+    public int removeFirst() {
         int result = head.getElement();
-       head= head.getNext();
+        head = head.getNext();
         return result;
     }
 
-   public  String toString(){
-      StringBuilder stringBuilder = new StringBuilder();
-      stringBuilder.append('[');
-      while (head!=null){
-          stringBuilder.append(head.getElement());
+    public void addLast(int element) {
+        Node node = new Node(element, null);
+        while (head.getNext() != null) {
+            head = head.getNext();
+        }
+        head.setNext(node);
+    }
 
-          head=head.getNext();
-          if (head!=null){
-              stringBuilder.append(", ");
-          }
-      }
-      stringBuilder.append(']');
-      return stringBuilder.toString();
-   }
+    public int getLast() {
+        while (head.getNext() != null) {
+            head = head.getNext();
+        }
+        return head.getElement();
+    }
+
+    public int removeLast() {
+        int result = 0;
+        if (head.getNext() == null) {
+            result = head.getElement();
+            head = null;
+        } else {
+            while (head.getNext() != null) {
+                if (head.getNext().getNext() == null) {
+                    result = head.getNext().getElement();
+                    head.setNext(null);
+                } else {
+                    head = head.getNext();
+                }
+            }
+        }
+        return result;
+
+    }
+
+
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append('[');
+        while (head != null) {
+            stringBuilder.append(head.getElement());
+
+            head = head.getNext();
+            if (head != null) {
+                stringBuilder.append(", ");
+            }
+        }
+        stringBuilder.append(']');
+        return stringBuilder.toString();
+    }
 
 }
--- src/test/java/collections/LinkedListTest.java
+++ src/test/java/collections/LinkedListTest.java
@@ -8,25 +8,66 @@ import static org.junit.jupiter.api.Assertions.*;
 class LinkedListTest {
 
     @Test
-    void addFirst(){
-        LinkedList linkedList =new LinkedList();
+    void addFirst() {
+        LinkedList linkedList = new LinkedList();
         linkedList.addFirst(10);
         linkedList.addFirst(13);
         linkedList.addFirst(3);
-        Assertions.assertEquals(3,linkedList.getFirst());
+        Assertions.assertEquals(3, linkedList.getFirst());
         linkedList.removeFirst();
-        Assertions.assertEquals(13,linkedList.getFirst());
+        Assertions.assertEquals(13, linkedList.getFirst());
     }
 
     @Test
-    void String(){
-            LinkedList linkedList =new LinkedList();
-            linkedList.addFirst(10);
-            linkedList.addFirst(13);
-            linkedList.addFirst(3);
-            Assertions.assertEquals("[3, 13, 10]",linkedList.toString());
+    void String() {
+        LinkedList linkedList = new LinkedList();
+        linkedList.addFirst(10);
+        linkedList.addFirst(13);
+        linkedList.addFirst(3);
+        Assertions.assertEquals("[3, 13, 10]", linkedList.toString());
 
-        }
+    }
+    @Test
+    void delete(){
+        LinkedList linkedList = new LinkedList();
+
+        linkedList.addFirst(3);
+
+        Assertions.assertEquals(3,linkedList.removeLast() );
+    }
+
+    @Test
+    void delet2e(){
+        LinkedList linkedList = new LinkedList();
+        linkedList.addFirst(10);
+        linkedList.addFirst(13);
+        linkedList.addFirst(3);
+        Assertions.assertEquals(10,linkedList.removeLast());
+    }
+    @Test
+    void getLast(){
+        LinkedList linkedList = new LinkedList();
+        linkedList.addFirst(555);
+        linkedList.addFirst(621);
+        linkedList.addFirst(10);
+        linkedList.addFirst(13);
+        linkedList.addFirst(3);
+        Assertions.assertEquals(555,linkedList.getLast());
+    }
+    @Test
+    void getLa3st(){
+        LinkedList linkedList = new LinkedList();
+        linkedList.addFirst(555);
+
+        Assertions.assertEquals(555,linkedList.getLast());
+    }
+    @Test
+    void addLast(){
+        LinkedList linkedList = new LinkedList();
+        linkedList.addFirst(555);
+        linkedList.addLast(31);
+        Assertions.assertEquals(31,linkedList.getLast());
+    }
 
 
 }
