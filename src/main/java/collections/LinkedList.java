079e8b5e3fb742d96b082d556f6c1217040d85ad
konstantin13-hi <kostya.chizhevskiy@mail.ru>
2022-07-07 23:16:08 +0200

    Node

--- src/main/java/collections/LinkedList.java
+++ src/main/java/collections/LinkedList.java
@@ -1,10 +1,51 @@
 package collections;
 
-import entities.Node;
+
+
 
 public class LinkedList {
     private Node head;
     private Node prev;
+
+
+    public static class Node {
+        public void setElement(int element) {
+            this.element = element;
+        }
+
+        private int element;
+        private Node next;
+        private Node prev;
+
+
+        public Node(int element, Node next, Node prev) {
+            this.element = element;
+            this.next = next;
+            this.prev = prev;
+        }
+
+        public void setPrev(Node prev) {
+            this.prev = prev;
+        }
+
+        public Node getPrev() {
+            return prev;
+        }
+
+        public int getElement() {
+            return element;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+
+    }
 
 
     public LinkedList() {
--- src/test/java/tasks/Task551Test.java
+++ src/test/java/tasks/Task551Test.java
@@ -1,16 +1,13 @@
 package tasks;
 
-import entities.Node;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class Task551Test {
 
     @Test
     void Build(){
-        Node node = Task551.buildLinkedList();
+        Task551.Node node = Task551.buildLinkedList();
         Assertions.assertEquals(12,node.getNext().getPrev().getElement());
         Assertions.assertEquals(99,node.getNext().getNext().getPrev().getNext().getPrev().getElement());
     }
