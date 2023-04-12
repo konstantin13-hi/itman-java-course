e386b3da764538058eca958ada23cfd856e27328
konstantin13-hi <kostya.chizhevskiy@mail.ru>
2023-04-13 00:04:07 +0200

    added IO exceptions,IoTask

--- src/test/java/tasks/IoTaskTest.java
+++ src/test/java/tasks/IoTaskTest.java
@@ -15,76 +15,76 @@ class IoTaskTest {
     public class IoTaskMain {
         @Test
         public void shouldReturnResultWhenFileContainsIntegers() throws IOException {
-                String[] strings = new String[1];
-                String path = new File("src" + File.separator + "test" + File.separator +
-                        "resources" + File.separator + "test.txt").getAbsolutePath();
-                strings[0] = path;
-                Assertions.assertEquals(returnsStrings(strings),
-                        path + " - 149.33");
+            String[] strings = new String[1];
+            String path = new File("src" + File.separator + "test" + File.separator +
+                    "resources" + File.separator + "test.txt").getAbsolutePath();
+            strings[0] = path;
+            Assertions.assertEquals(returnsStrings(strings),
+                    path + " - 149.33");
         }
 
 
         @Test
         public void shouldReturnResultWhenFileContainsDoubles() throws IOException {
-                String[] strings = new String[1];
-                String path = new File("src" + File.separator + "test" + File.separator +
-                        "resources" + File.separator + "testSecond.txt").getAbsolutePath();
-                strings[0] = path;
-                Assertions.assertEquals(returnsStrings(strings), strings[0] + " - 27.77");
+            String[] strings = new String[1];
+            String path = new File("src" + File.separator + "test" + File.separator +
+                    "resources" + File.separator + "testSecond.txt").getAbsolutePath();
+            strings[0] = path;
+            Assertions.assertEquals(returnsStrings(strings), strings[0] + " - 27.77");
         }
 
 
         @Test
         public void shouldReturnErrorWhenFileContainsString() throws IOException {
-                String[] strings = new String[1];
-                String path = new File("src" + File.separator + "test" + File.separator +
-                        "resources" + File.separator + "testFour.txt").getAbsolutePath();
-                strings[0] = path;
-                Assertions.assertEquals(returnsStrings(strings), strings[0] +
-                        " - Error on line 1.Incorrect number ref");
+            String[] strings = new String[1];
+            String path = new File("src" + File.separator + "test" + File.separator +
+                    "resources" + File.separator + "testFour.txt").getAbsolutePath();
+            strings[0] = path;
+            Assertions.assertEquals(returnsStrings(strings), strings[0] +
+                    " - Error on line 1.Incorrect number ref");
         }
 
         @Test
         public void shouldReturnErrorWhenFileContainsSpace() throws IOException {
-                String[] strings = new String[1];
-                String path = new File("src" + File.separator + "test" + File.separator +
-                        "resources" + File.separator + "testThird.txt").getAbsolutePath();
-                strings[0] = path;
-                Assertions.assertEquals(returnsStrings(strings), strings[0] +
-                        " - Error on line 2.Empty line,not found number");
+            String[] strings = new String[1];
+            String path = new File("src" + File.separator + "test" + File.separator +
+                    "resources" + File.separator + "testThird.txt").getAbsolutePath();
+            strings[0] = path;
+            Assertions.assertEquals(returnsStrings(strings), strings[0] +
+                    " - Error on line 2.Empty line,not found number");
         }
 
         @Test
         public void shouldReturnErrorWhenFileDoesntExist() throws IOException {
-                String[] strings = new String[1];
-                strings[0] = "testNotExists.txt";
-                Assertions.assertEquals(returnsStrings(strings), strings[0] +
-                        " - Incorrect input path");
+            String[] strings = new String[1];
+            strings[0] = "testNotExists.txt";
+            Assertions.assertEquals(returnsStrings(strings), strings[0] +
+                    " - Incorrect input path");
         }
 
 
         @Test
         public void shouldReturnResultsWhenSeveralFiles() throws IOException {
-                String[] strings = new String[5];
-                String path = new File("src" + File.separator + "test" + File.separator +
-                        "resources" + File.separator + "test.txt").getAbsolutePath();
-                strings[0] = path;
-                String pathSecond = new File("src" + File.separator + "test" + File.separator +
-                        "resources" + File.separator + "testSecond.txt").getAbsolutePath();
-                strings[1] = pathSecond;
-                String pathThird = new File("src" + File.separator + "test" + File.separator +
-                        "resources" + File.separator + "testThird.txt").getAbsolutePath();
-                strings[2] = pathThird;
-                String pathFourth = new File("src" + File.separator + "test" + File.separator +
-                        "resources" + File.separator + "testFour.txt").getAbsolutePath();
-                strings[3] = pathFourth;
-                strings[4] = "testNotExists.txt";
-                Assertions.assertEquals(returnsStrings(strings),
-                        strings[0] + " - 149.33" + "\n" +
-                                strings[1] + " - 27.77" + "\n" +
-                                strings[2] + " - Error on line 2.Empty line,not found number" + "\n" +
-                                strings[3] + " - Error on line 1.Incorrect number ref" + "\n" +
-                                strings[4] + " - Incorrect input path");
+            String[] strings = new String[5];
+            String path = new File("src" + File.separator + "test" + File.separator +
+                    "resources" + File.separator + "test.txt").getAbsolutePath();
+            strings[0] = path;
+            String pathSecond = new File("src" + File.separator + "test" + File.separator +
+                    "resources" + File.separator + "testSecond.txt").getAbsolutePath();
+            strings[1] = pathSecond;
+            String pathThird = new File("src" + File.separator + "test" + File.separator +
+                    "resources" + File.separator + "testThird.txt").getAbsolutePath();
+            strings[2] = pathThird;
+            String pathFourth = new File("src" + File.separator + "test" + File.separator +
+                    "resources" + File.separator + "testFour.txt").getAbsolutePath();
+            strings[3] = pathFourth;
+            strings[4] = "testNotExists.txt";
+            Assertions.assertEquals(returnsStrings(strings),
+                    strings[0] + " - 149.33" + "\n" +
+                            strings[1] + " - 27.77" + "\n" +
+                            strings[2] + " - Error on line 2.Empty line,not found number" + "\n" +
+                            strings[3] + " - Error on line 1.Incorrect number ref" + "\n" +
+                            strings[4] + " - Incorrect input path");
         }
     }
 
