4ef041eba3072fdcfb6c23d61044036ae800a985
konstantin13-hi <kostya.chizhevskiy@mail.ru>
2023-04-12 22:51:19 +0200

    added IO exceptions,IoTask

--- src/test/java/tasks/IoTaskTest.java
+++ src/test/java/tasks/IoTaskTest.java
@@ -14,9 +14,15 @@ class IoTaskTest {
     public class IoTaskMain {
         @Test
         public void shouldReturnResultWhenFileContainsIntegers() throws IOException {
-            String[] strings = new String[1];
-            strings[0] = "test.txt";
-            Assertions.assertEquals(returnsStrings(strings), "test.txt - 149.33");
+            try {
+
+
+                String[] strings = new String[1];
+                strings[0] = "test.txt";
+                Assertions.assertEquals(returnsStrings(strings), "test.txt - 149.33");
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
 
 
         }
@@ -24,9 +30,14 @@ class IoTaskTest {
 
         @Test
         public void shouldReturnResultWhenFileContainsDoubles() throws IOException {
-            String[] strings = new String[1];
-            strings[0] = "testSecond.txt";
-            Assertions.assertEquals(returnsStrings(strings), strings[0] + " - 27.77");
+            try {
+
+                String[] strings = new String[1];
+                strings[0] = "testSecond.txt";
+                Assertions.assertEquals(returnsStrings(strings), strings[0] + " - 27.77");
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
 
 
         }
@@ -34,46 +45,66 @@ class IoTaskTest {
 
         @Test
         public void shouldReturnErrorWhenFileContainsString() throws IOException {
-            String[] strings = new String[1];
-            strings[0] = "testFour.txt";
-            Assertions.assertEquals(returnsStrings(strings), strings[0] + " - Error on line 1.Incorrect number ref");
+            try {
+
+
+                String[] strings = new String[1];
+                strings[0] = "testFour.txt";
+                Assertions.assertEquals(returnsStrings(strings), strings[0] + " - Error on line 1.Incorrect number ref");
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
 
 
         }
 
         @Test
         public void shouldReturnErrorWhenFileContainsSpace() throws IOException {
-            String[] strings = new String[1];
-            strings[0] = "testThird.txt";
-            Assertions.assertEquals(returnsStrings(strings), strings[0] +
-                    " - Error on line 2.Empty line,not found number");
-
+            try {
+                String[] strings = new String[1];
+                strings[0] = "testThird.txt";
+                Assertions.assertEquals(returnsStrings(strings), strings[0] +
+                        " - Error on line 2.Empty line,not found number");
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
 
         }
 
         @Test
         public void shouldReturnErrorWhenFileDoesntExist() throws IOException {
-            String[] strings = new String[1];
-            strings[0] = "testNotExists.txt";
-            Assertions.assertEquals(returnsStrings(strings), strings[0] +
-                    " - Incorrect input path");
+            try {
+
+                String[] strings = new String[1];
+                strings[0] = "testNotExists.txt";
+                Assertions.assertEquals(returnsStrings(strings), strings[0] +
+                        " - Incorrect input path");
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
         }
 
 
         @Test
         public void shouldReturnResultsWhenSeveralFiles() throws IOException {
-            String[] strings = new String[5];
-            strings[0] = "test.txt";
-            strings[1] = "testSecond.txt";
-            strings[2] = "testThird.txt";
-            strings[3] = "testFour.txt";
-            strings[4] = "testNotExists.txt";
-            Assertions.assertEquals(returnsStrings(strings),
-                    strings[0] + " - 149.33" + "\n" +
-                            strings[1] + " - 27.77" + "\n" +
-                            strings[2] + " - Error on line 2.Empty line,not found number" + "\n" +
-                            strings[3] + " - Error on line 1.Incorrect number ref" + "\n" +
-                            strings[4] + " - Incorrect input path");
+            try {
+
+
+                String[] strings = new String[5];
+                strings[0] = "test.txt";
+                strings[1] = "testSecond.txt";
+                strings[2] = "testThird.txt";
+                strings[3] = "testFour.txt";
+                strings[4] = "testNotExists.txt";
+                Assertions.assertEquals(returnsStrings(strings),
+                        strings[0] + " - 149.33" + "\n" +
+                                strings[1] + " - 27.77" + "\n" +
+                                strings[2] + " - Error on line 2.Empty line,not found number" + "\n" +
+                                strings[3] + " - Error on line 1.Incorrect number ref" + "\n" +
+                                strings[4] + " - Incorrect input path");
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
         }
     }
 
